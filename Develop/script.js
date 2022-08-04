d9c4e8136caf848e8a8f65165c78b90658bc280c
Nicholas148894 <nich.oettinger@gmail.com>
2022-08-04 15:25:21 -0400

    skeleton reorganized and filled in. Currently an error related to a function I am working on solving

--- Develop/script.js
+++ Develop/script.js
@@ -7,32 +7,53 @@ var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 var numbers = "1234567890".split("")
 var special = "!?@#$%^&*()".split("")
 var length = 128
+var fullArray = []
 var password = ""
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
 
 
 // Write password to the #password input
 function writePassword() {
-  var makeLower = confirm("Would you like lowercase in your password?")
-  var makeUpper = confirm("Would you like uppercase in your password?")
-  var makeNumber = confirm("Would you like numbers in your password?")
-  var makespecial = confirm("Would you like special characters in your password?")
-  var minimumChar = 8
 
-  var lowerPoss = Math.floor(Math.random() + 155)
-  var lowerChoice = lowers[lowerPoss]
-  var upperChoice = Math.floor
-
-
-  if (makeLower === true) {
-    
-  }
-  
   var generatePassword = generatePassword();
   var passwordText = document.querySelector("#password");
   
-  passwordText.value = password;
-  
+passwordText.value = password;
 }
+
+// This is the part that creates a password
+function generatePassword() {
+
+var passwordLength = window.prompt("How many characters long would you like your password to be?")
+if (passwordLength < 8 || passwordLength > 156) {
+  alert("Insert valid number")
+} else  {
+  var makeLower = confirm("Would you like lowercase in your password?")
+  var makeUpper = confirm("Would you like uppercase in your password?")
+  var makeNumber = confirm("Would you like numbers in your password?")
+  var makeSpecial = confirm("Would you like special characters in your password?")
+  if (makeLower = true) {
+    fullArray = fullArray.concat(makeLower)
+  }
+  if (makeUpper = true) {
+    fullArray = fullArray.concat(makeUpper)
+  }
+  if (makeNumber = true) {
+    fullArray = fullArray.concat(makeNumber)
+  }
+  if (makeSpecial = true) {
+    fullArray = fullArray.concat(makeSpecial)
+  }
+
+  for (var i=0; i < passwordLength; i++) {
+    currentCharacter = Math.floor(Math.random() * fullArray.length)
+    password += currentCharacter
+  }
+
+  return password
+  }
+} 
+
+
