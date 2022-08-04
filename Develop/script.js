2d2208728df0207c040881a2cec8c18fbb6df48d
Nicholas148894 <nich.oettinger@gmail.com>
2022-08-04 17:18:39 -0400

    setting up deployment before resolving my code issues

--- Develop/script.js
+++ Develop/script.js
@@ -13,17 +13,8 @@ var password = ""
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
 
-
-// Write password to the #password input
-function writePassword() {
-
-  var generatePassword = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-passwordText.value = password;
-}
-
 // This is the part that creates a password
+
 function generatePassword() {
 
 var passwordLength = window.prompt("How many characters long would you like your password to be?")
@@ -34,7 +25,7 @@ if (passwordLength < 8 || passwordLength > 156) {
   var makeUpper = confirm("Would you like uppercase in your password?")
   var makeNumber = confirm("Would you like numbers in your password?")
   var makeSpecial = confirm("Would you like special characters in your password?")
-  if (makeLower = true) {
+   if (makeLower = true) {
     fullArray = fullArray.concat(makeLower)
   }
   if (makeUpper = true) {
@@ -46,14 +37,22 @@ if (passwordLength < 8 || passwordLength > 156) {
   if (makeSpecial = true) {
     fullArray = fullArray.concat(makeSpecial)
   }
-
+}
   for (var i=0; i < passwordLength; i++) {
     currentCharacter = Math.floor(Math.random() * fullArray.length)
     password += currentCharacter
   }
 
   return password
-  }
+  
 } 
 
 
+// Write password to the #password input
+function writePassword() {
+
+  generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+passwordText.value = password;
+}
